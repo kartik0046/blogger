9cb487c26d7c03c0ee24c8fe430229e2c6206805
KartikTakyar0046 <42770126+KartikTakyar0046@users.noreply.github.com>
2021-10-23 17:13:36 +0530

    for user login and settings

--- posts/forms.py
+++ posts/forms.py
@@ -9,3 +9,20 @@ class AddAnswer(ModelForm):
     class Meta:
         model=Answer
         fields=['body']
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from django import forms
+from .models import *
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','email','password1','password2']
+
+
+class AccountSettings(ModelForm):
+    class Meta:
+        model=Profile
+        fields='__all__'
+        exclude=['user','email']
